a8f44616b570a9a4b42ace3d3e9b78a84706c766
Quanny Hampton <younghampton@gmail.com>
2017-10-04 12:34:19 -0400

    Update Doors.java

--- Doors.java
+++ Doors.java
@@ -1,24 +1,34 @@
-import java.util.Arrays;
-
-
-public class Doors {
-
-    public Doors(int len) {
-        this.doorStatus = new int[len];      
-    }
-
-    public int[] answer(int[] d, int pass) {
-        if (pass == d.length) {
-            d[d.length-1] ^= 1;
-            return this.doorStatus = d;
-        } else {
-            for(int i=pass-1; i<d.length; i=i+pass) {
-                d[i] ^= 1;
-            }
-
-            return this.answer(d, pass+1);
-        }
-    }
-
-    private int[] doorStatus;
+public class HundredDoors {
+	
+	public static void main(String[] args) {
+		boolean[] doors = new boolean[101];
+		
+		for (int j = 1; j <= 100; j++) {
+			checkAllDoors(j, doors);
+		}
+		
+		
+		printDoors(doors);
+		
+	}
+	
+	public static void checkAllDoors(int steps, boolean[] doors) {
+		
+		for(int j = 0; j < doors.length; j+=steps) {
+			doors[j] = !doors[j];
+		}
+		
+	}
+	
+	public static void printDoors(boolean[] doors) {
+		for(int k = 1; k < doors.length; k++) {
+			if(doors[k]) {
+				System.out.printf("Door %d is Open \n", k);	
+			}else{
+				System.out.printf("Door %d is Closed \n", k);
+			}
+			
+			
+		}
+	}
 }
